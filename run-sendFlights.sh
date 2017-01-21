f0a3826ac4f89b231654e6f0eeb97a545322d0b5
Nick Balkissoon <ngbalk@gmail.com>
2017-01-21 18:02:44 +0000

    changed script

--- run-sendFlights.sh
+++ run-sendFlights.sh
@@ -2,9 +2,13 @@
 
 # run the send flights information once every 24 hours
 
+
+echo 'starting script'
 while true
 do 
-    timeout --signal=SIGINT 40 /Users/jreshef/Documents/GitHub/flights-genie/run.sh
-    sleep 180
-    # sleep 86400
+    echo 'calling run script'
+    timeout --signal=SIGINT 40 sh /opt/genie/flights-genie/run.sh
+    echo 'script ran sleeping now'
+    sleep 28000
+    # sleep 86400 -- 24 hours
 done
